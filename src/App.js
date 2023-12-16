98442ac5d152523a7c6f3e5900abfa2f58af724b
raabin10 <raabinsinghasokkumar@gmail.com>
2023-12-16 20:26:25 +0530

    my first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () =>{
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const userEmail = document.getElementById("user-mail");
+    // const userPassword = document.getElementById("user-password");
+    // const userPasswordConfirm = document.getElementById("user-password-confirm");
+
+    // if(email.endsWith("@gmail.com")){
+    //   userEmail.style.borderColor = 'green'
+    // }
+
+    
+    function handleSubmit(){
+      if((email.endsWith("@gmail.com")) && (password.length>= 8) && (password == confirmPassword)){
+        alert("User form submitted");
+      }else if(password != confirmPassword){
+        alert("password not match")
+      }
+    }
+    
+
+    return(
+      <form id="main-form" onSubmit={handleSubmit}>
+
+        <div className="field" id="email">
+          <label><b>Email:</b></label><br/>
+          <input 
+            type="email" 
+            id="user-email" 
+            name="email" 
+            required
+            onChange={e =>{setEmail(e.target.value)}}>
+          </input>
+          {
+            email.endsWith("@gmail.com") ?  <p></p> : <p className="text">Invalid email format</p>
+          }
+          {/* {
+            email.endsWith("@gmail.com") ?  userEmail.style.borderColor = 'tomato': userEmail.style.borderColor = "green" 
+          } */}
+        </div>
+
+        <div className="field" id="password">
+          <label><b>Password:</b></label><br/>
+          <input 
+            type="password" 
+            id="user-password" 
+            name="email" 
+            required
+            onChange={e =>{setPassword(e.target.value)}}>
+          </input>
+          {
+            (password.length>= 8) ? <p></p> : <p className="text">Password must be at least 8 characters</p> 
+          }
+          {/* {
+            (password.length>= 8) ?  userPasswordConfirm.style.borderColor = "tomato": userPasswordConfirm.style.borderColor = "green"
+          } */}
+          
+        </div>
+
+        <div className="field" id="confirm-password">
+          <label><b>Confirm Password:</b></label><br/>
+          <input 
+            type="password" 
+            id="user-password-confirm" 
+            name="email" 
+            required
+            onChange={e =>{setConfirmPassword(e.target.value)}}>
+          </input>
+          {
+            (password == confirmPassword) && (password!="") ? <p></p> : <p className="text">Password do not match</p> 
+          }
+          {/* {
+            (password == confirmPassword) && (password!="") ?
+             userPasswordConfirm.style.borderColor = "tomato" : userPasswordConfirm.style.borderColor = "green"
+          } */}
+        </div>
+
+        <div className="field" id="btn-container">
+          <button id="signup-btn">Sign Up</button>
+        </div>
+        
+      </form>   
+    )
 }
 
 export default App;
